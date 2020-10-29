98c9cabd132ffa43184dfed6a681fb5358e40748
Denis Efremov <efremov@linux.com>
2020-10-29 11:30:07 +0300

    import script update
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- import.sh
+++ import.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+set -e
+
 CWD=$(pwd)
+
+model="$(echo "$1" | cut -d '-' -f 2)"
 
 rm -fr *
 unzip "$1" Kernel.tar.gz
 tar xf Kernel.tar.gz
 rm Kernel.tar.gz
 
+
 chmod -x arch/arm64/configs/*
+chmod -x drivers/misc/tzdev/startup.tzar
+chmod -x firmware/tsp_sec/y761_beyond0_phole.bin
+chmod -x drivers/gator/mali_midgard.mk
+chmod -x drivers/net/wireless/broadcom/bcmdhd_100_10/Makefile.kk
+chmod -x drivers/net/wireless/broadcom/bcmdhd_100_10/Makefile.lp
+chmod -x drivers/net/ethernet/cadence/macb_ptp.c
+chmod -x drivers/gator/COPYING
+
 find . -type f \( -name '*.dts' -o -name '*.dtsi' -o -name '*.[ch]' -o -name '*.ihex' -o -name Kbuild -o -name Kconfig -o -name Makefile \) -executable -print0 | xargs -0 -n5000 -P$(nproc) chmod -x
 
 #git checkout `git status | grep deleted | cut -d ':' -f 2 | cut -d ' ' -f 5`
@@ -16,14 +29,22 @@ find . -type f -print | fgrep -v './firmware' | fgrep -v './.git' | fgrep -v './
 
 git checkout `git diff --no-color | fgrep --color=never -B 2 -e "new mode" | fgrep --color=never diff | cut -d '/' -f 2- | cut -d ' ' -f 1`
 
-find . -type f -name '*.bin' -exec "$CWD"/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin/aarch64-linux-android-objcopy -I binary -O ihex '{}' '{}'.ihex \;
+objdump="$CWD"/scripts/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin/aarch64-linux-android-objcopy
 
-find . -type f -name '*.fw' -exec "$CWD"/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin/aarch64-linux-android-objcopy -I binary -O ihex '{}' '{}'.ihex \;
+if [[ ! -f $objdump ]]; then
+	objdump="$CWD"/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin/aarch64-linux-android-objcopy
+fi
+
+$objdump --version
+
+find . -type f -name '*.bin' -exec $objdump -I binary -O ihex '{}' '{}'.ihex \;
+find . -type f -name '*.fw'  -exec $objdump -I binary -O ihex '{}' '{}'.ihex \;
 
 sed -i -e 's/\r/\n/g' drivers/video/fbdev/exynos/dpu20/mcd_hdr/mcd_cm_def.h
 sed -i -e 's/\r/\n/g' drivers/video/fbdev/exynos/dpu20/mcd_hdr/mcd_cm_lut.h
 
 git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \grep arm64 | \grep boot | \grep dts | \grep -v samsung | \grep -v exynos)
+#git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \grep arm64 | \grep boot | \grep dts)
 
 git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \grep Documentation)
 
@@ -39,4 +60,18 @@ git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5-
 
 git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \fgrep 'w9020_davinci.fw.ihex')
 
+git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \fgrep 'build.bp')
+
+git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \fgrep 'tg3')
+
+git checkout $(\git status | \grep deleted | cut -d ':' -f 1- | cut -d ' ' -f 5- | \fgrep 'clang-android')
+
+mv arch/arm64/boot/dts/Makefile arch/arm64/boot/dts/$model.mk
+#mv arch/arm64/boot/dts/exynos/Makefile arch/arm64/boot/dts/exynos/$model.mk
+
+rm -f security/defex_lsm/file_list
+rm -f security/defex_lsm/build_defex_log
+
 rm -fr security/samsung/defex_lsm/build_defex_log security/dsms/ security/defex_lsm/
+#rm -fr scripts/toolchain
+#rm -fr toolchain
